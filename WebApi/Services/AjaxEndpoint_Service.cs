6622985aa89230feb2b38c9fac82fa69e55aff8f
EthanShen-topocean <ethanshen@topocean.com.cn>
2022-06-29 16:39:20 +0800

    fix something

---
+++ WebApi/Services/AjaxEndpoint_Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.IServices;
+
+namespace WebApi.Services
+{
+    public class AjaxEndpoint_Service : IAjaxEndpoint
+    {
+
+    }
+}
